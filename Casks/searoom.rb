a204f30d2df94645c4ee113ec8556d266fa1bf05
hamza <hamza7x7@gmail.com>
2026-09-03 02:06:37 +0100

    Update to 0.3.0

--- Casks/searoom.rb
+++ Casks/searoom.rb
@@ -1,6 +1,6 @@
 cask "searoom" do
-  version "0.2.0"
-  sha256 "afc3a9aa84d5c1635405a6b02dc1f01811246ce6919569be01e545f6db6eeddb"
+  version "0.3.0"
+  sha256 "22fcc6090716766789183ccc0ba9f535b4f28732904b12776a1366bd62f42661"
 
   url "https://github.com/emaitchess/searoom/releases/download/v#{version}/Searoom.dmg",
       verified: "github.com/emaitchess/searoom/"
